252a49839134f76c0baa8a94cbfcdc0a37cb2764
N-Nagorny <nikiton-n@mail.ru>
2022-04-20 00:37:18 +0300

    Add for_each_mode() algorithm

--- include/edid/timing_modes.hh
+++ include/edid/timing_modes.hh
@@ -3,6 +3,7 @@
 #include <map>
 
 #include "base_block.hh"
+#include "cta861_block.hh"
 
 namespace Edid {
   struct VideoTimingMode {
@@ -44,4 +45,66 @@ namespace Edid {
   };
 
   Cta861VideoTimingMode get_cta861_video_timing_mode(uint8_t vic);
+
+  template <class Function>
+  void for_each_mode(const BaseBlock& base_block, Function fn) {
+    for (EstablishedTiming1 et : bitfield_to_enums<EstablishedTiming1>(base_block.established_timings_1)) {
+      auto mode = established_timings_1.at(et);
+      fn(mode.h_res, mode.v_res, std::make_pair(mode.v_rate_hz, 1), mode.interlaced);
+    }
+    for (EstablishedTiming2 et : bitfield_to_enums<EstablishedTiming2>(base_block.established_timings_2)) {
+      auto mode = established_timings_2.at(et);
+      fn(mode.h_res, mode.v_res, std::make_pair(mode.v_rate_hz, 1), mode.interlaced);
+    }
+    for (EstablishedTiming3 et : bitfield_to_enums<EstablishedTiming3>(base_block.established_timings_3)) {
+      auto mode = established_timings_3.at(et);
+      fn(mode.h_res, mode.v_res, std::make_pair(mode.v_rate_hz, 1), mode.interlaced);
+    }
+
+    for (const auto& mode : base_block.detailed_timing_descriptors) {
+      if (mode.has_value()) {
+        fn(
+          mode->h_res,
+          mode->v_res * (mode->features_bitmap.interlaced ? 2 : 1),
+          std::make_pair(mode->pixel_clock_hz, (mode->v_res + mode->v_blank_lines) * (mode->h_res + mode->h_blank_pixels)),
+          mode->features_bitmap.interlaced
+        );
+      }
+    }
+  }
+
+  template <class Function>
+  void for_each_mode(const Cta861Block& cta861_block, Function fn) {
+    for (const std::optional<uint8_t>& vic : cta861_block.data_block_collection.video_data_block.vics) {
+      if (vic.has_value()) {
+        auto mode = get_cta861_video_timing_mode(vic.value());
+        uint8_t pixel_repetition_factor = 1;
+        if (std::holds_alternative<uint8_t>(mode.pixel_repetition_factor)) {
+          pixel_repetition_factor = std::get<uint8_t>(mode.pixel_repetition_factor);
+        }
+        else if (std::holds_alternative<std::pair<uint8_t, uint8_t>>(mode.pixel_repetition_factor)) {
+          pixel_repetition_factor = std::get<std::pair<uint8_t, uint8_t>>(mode.pixel_repetition_factor).first;
+        }
+        else if (std::holds_alternative<std::vector<uint8_t>>(mode.pixel_repetition_factor)) {
+          pixel_repetition_factor = std::get<std::vector<uint8_t>>(mode.pixel_repetition_factor).at(0);
+        }
+
+        fn(
+          mode.dtd.h_res / pixel_repetition_factor,
+          mode.dtd.v_res * (mode.dtd.features_bitmap.interlaced ? 2 : 1),
+          std::make_pair(mode.dtd.pixel_clock_hz, (mode.dtd.v_res + mode.dtd.v_blank_lines) * (mode.dtd.h_res + mode.dtd.h_blank_pixels)),
+          mode.dtd.features_bitmap.interlaced
+        );
+      }
+    }
+
+    for (const auto& mode : cta861_block.detailed_timing_descriptors) {
+      fn(
+        mode.h_res,
+        mode.v_res * (mode.features_bitmap.interlaced ? 2 : 1),
+        std::make_pair(mode.pixel_clock_hz, (mode.v_res + mode.v_blank_lines) * (mode.h_res + mode.h_blank_pixels)),
+        mode.features_bitmap.interlaced
+      );
+    }
+  }
 }
--- src/bcp00501.cc
+++ src/bcp00501.cc
@@ -43,70 +43,13 @@ namespace Edid {
     return result;
   }
 
-  std::unordered_set<json> generate_constraint_sets(const BaseBlock& base_block) {
+  template <class Block>
+  std::unordered_set<json> generate_constraint_sets(const Block& block) {
     std::unordered_set<json> result;
 
-    for (EstablishedTiming1 et : bitfield_to_enums<EstablishedTiming1>(base_block.established_timings_1)) {
-      auto mode = established_timings_1.at(et);
-      result.insert(generate_constraint_set(mode.h_res, mode.v_res, std::make_pair(mode.v_rate_hz, 1), mode.interlaced));
-    }
-    for (EstablishedTiming2 et : bitfield_to_enums<EstablishedTiming2>(base_block.established_timings_2)) {
-      auto mode = established_timings_2.at(et);
-      result.insert(generate_constraint_set(mode.h_res, mode.v_res, std::make_pair(mode.v_rate_hz, 1), mode.interlaced));
-    }
-    for (EstablishedTiming3 et : bitfield_to_enums<EstablishedTiming3>(base_block.established_timings_3)) {
-      auto mode = established_timings_3.at(et);
-      result.insert(generate_constraint_set(mode.h_res, mode.v_res, std::make_pair(mode.v_rate_hz, 1), mode.interlaced));
-    }
-
-    for (const auto& mode : base_block.detailed_timing_descriptors) {
-      if (mode.has_value()) {
-        result.insert(generate_constraint_set(
-          mode->h_res,
-          mode->v_res * (mode->features_bitmap.interlaced ? 2 : 1),
-          std::make_pair(mode->pixel_clock_hz, (mode->v_res + mode->v_blank_lines) * (mode->h_res + mode->h_blank_pixels)),
-          mode->features_bitmap.interlaced
-        ));
-      }
-    }
-
-    return result;
-  }
-
-  std::unordered_set<json> generate_constraint_sets(const Cta861Block& cta861_block) {
-    std::unordered_set<json> result;
-
-    for (const std::optional<uint8_t>& vic : cta861_block.data_block_collection.video_data_block.vics) {
-      if (vic.has_value()) {
-        auto mode = get_cta861_video_timing_mode(vic.value());
-        uint8_t pixel_repetition_factor = 1;
-        if (std::holds_alternative<uint8_t>(mode.pixel_repetition_factor)) {
-          pixel_repetition_factor = std::get<uint8_t>(mode.pixel_repetition_factor);
-        }
-        else if (std::holds_alternative<std::pair<uint8_t, uint8_t>>(mode.pixel_repetition_factor)) {
-          pixel_repetition_factor = std::get<std::pair<uint8_t, uint8_t>>(mode.pixel_repetition_factor).first;
-        }
-        else if (std::holds_alternative<std::vector<uint8_t>>(mode.pixel_repetition_factor)) {
-          pixel_repetition_factor = std::get<std::vector<uint8_t>>(mode.pixel_repetition_factor).at(0);
-        }
-
-        result.insert(generate_constraint_set(
-          mode.dtd.h_res / pixel_repetition_factor,
-          mode.dtd.v_res * (mode.dtd.features_bitmap.interlaced ? 2 : 1),
-          std::make_pair(mode.dtd.pixel_clock_hz, (mode.dtd.v_res + mode.dtd.v_blank_lines) * (mode.dtd.h_res + mode.dtd.h_blank_pixels)),
-          mode.dtd.features_bitmap.interlaced
-        ));
-      }
-    }
-
-    for (const auto& mode : cta861_block.detailed_timing_descriptors) {
-      result.insert(generate_constraint_set(
-        mode.h_res,
-        mode.v_res * (mode.features_bitmap.interlaced ? 2 : 1),
-        std::make_pair(mode.pixel_clock_hz, (mode.v_res + mode.v_blank_lines) * (mode.h_res + mode.h_blank_pixels)),
-        mode.features_bitmap.interlaced
-      ));
-    }
+    for_each_mode(block, [&result](uint16_t frame_width, uint16_t frame_height, Ratio frame_rate, bool interlaced) {
+      result.insert(generate_constraint_set(frame_width, frame_height, frame_rate, interlaced));
+    });
 
     return result;
   }
